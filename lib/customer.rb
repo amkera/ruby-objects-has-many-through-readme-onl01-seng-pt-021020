47c2f20a65477871641b9d83c3c01b8335755735
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-03-10 21:47:53 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,16 @@
+require 'pry'
 class Customer
+  attr_accessor :name, :age 
+  
+  @@all = [] #all customers 
+  
+  def initialize(name, age)
+    @name = name
+    @age = age 
+    @@all << self 
+    binding.pry
+  end 
+  
+  
   
 end
